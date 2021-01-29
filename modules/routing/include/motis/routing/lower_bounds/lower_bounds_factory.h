6170b95565ef6e130c4bb5eba59e190a9a6c49f3
Florian Beutel <florian.beutel@stud.tu-darmstadt.de>
2021-01-29 12:22:56 +0100

    Fixed compiler warning of non-void function not returning

--- modules/routing/include/motis/routing/lower_bounds/lower_bounds_factory.h
+++ modules/routing/include/motis/routing/lower_bounds/lower_bounds_factory.h
@@ -45,14 +45,16 @@ std::unique_ptr<lower_bounds<Label>> combine_with_price(
     search_query const& q, std::vector<int> const& goal_ids, search_dir dir,
     LowerBoundsTravelTime& lb_tt, LowerBoundsTransfers& lb_tr) {
   switch (q.lb_price_type_) {
-    case lower_bounds_price_type::NONE: {
-      lower_bounds_none<Label> lbs_none = lower_bounds_none<Label>(q, dir);
-      return combine_with_optimality<Label>(q, dir, lb_tt, lb_tr, lbs_none);
-    }
+
     case lower_bounds_price_type::DISTANCE: {
       lower_bounds_price<Label> lbs_dist =
           lower_bounds_price<Label>(q, goal_ids);
       return combine_with_optimality<Label>(q, dir, lb_tt, lb_tr, lbs_dist);
+    }
+    case lower_bounds_price_type::NONE:
+    default: {
+      lower_bounds_none<Label> lbs_none = lower_bounds_none<Label>(q, dir);
+      return combine_with_optimality<Label>(q, dir, lb_tt, lb_tr, lbs_none);
     }
   }
 }
@@ -62,14 +64,15 @@ std::unique_ptr<lower_bounds<Label>> combine_with_transfers(
     search_query const& q, std::vector<int> const& goal_ids, search_dir dir,
     LowerBoundsTravelTime& lb_tt) {
   switch (q.lb_transfers_type_) {
-    case lower_bounds_transfers_type::NONE: {
-      lower_bounds_none<Label> lbs_none = lower_bounds_none<Label>(q, dir);
-      return combine_with_price<Label>(q, goal_ids, dir, lb_tt, lbs_none);
-    }
     case lower_bounds_transfers_type::CG: {
       lower_bounds_transfers<Label> lbs_cg =
           lower_bounds_transfers<Label>(q, dir, goal_ids);
       return combine_with_price<Label>(q, goal_ids, dir, lb_tt, lbs_cg);
+    }
+    case lower_bounds_transfers_type::NONE:
+    default: {
+      lower_bounds_none<Label> lbs_none = lower_bounds_none<Label>(q, dir);
+      return combine_with_price<Label>(q, goal_ids, dir, lb_tt, lbs_none);
     }
   }
 }
@@ -81,15 +84,18 @@ lower_bounds_result<Label> get_lower_bounds_for_query(
   std::unique_ptr<lower_bounds<Label>> lbs;
   lower_bounds_result<Label> result{};
   switch (q.lb_travel_time_type_) {
-    case lower_bounds_travel_time_type::NONE: {
-      lower_bounds_none<Label> lbs_none = lower_bounds_none<Label>(q, dir);
-      lbs = combine_with_transfers<Label>(q, goal_ids, dir, lbs_none);
-      break;
-    }
+
     case lower_bounds_travel_time_type::CG: {
       lower_bounds_travel_time<Label> lbs_cg =
           lower_bounds_travel_time<Label>(q, dir, goal_ids);
       lbs = combine_with_transfers<Label>(q, goal_ids, dir, lbs_cg);
+      break;
+    }
+    case lower_bounds_travel_time_type::NONE:
+    default: {
+      lower_bounds_none<Label> lbs_none = lower_bounds_none<Label>(q, dir);
+      lbs = combine_with_transfers<Label>(q, goal_ids, dir, lbs_none);
+      break;
     }
   }
 
